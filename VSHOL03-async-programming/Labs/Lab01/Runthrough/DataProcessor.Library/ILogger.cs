0897ca8f52c667dca0578b1591fd0ddd7f4939f3
Jeremy Clark <jeremy@jeremybytes.com>
2022-10-24 09:19:07 -0700

    Lab cleanup

--- VSHOL03-async-programming/Labs/Lab01/Runthrough/DataProcessor.Library/ILogger.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DataProcessor.Library;
-
-public interface ILogger
-{
-    Task LogMessage(string message, string data);
-}
--- VSHOL03-async-programming/Labs/Lab02/Runthrough/DataProcessor.Library.Tests/FakeLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataProcessor.Library.Tests;
-
-public class FakeLogger : ILogger
-{
-    public Task LogMessage(string message, string data)
-    {
-        return Task.CompletedTask;
-    }
-}
